d00e2b4d8434cbf726565f77ff6e3d7a00972e47
MateuszKozlowski1124 <70546149+MateuszKozlowski1124@users.noreply.github.com>
2022-06-05 12:38:27 +0200

    Revert "Move loading data to App::loadData()"
    This reverts commit 5701d5aebce832346e0cf5d274bf4ae50d6b31b7.

--- Small Neural Net Visualization/src/App.cpp
+++ Small Neural Net Visualization/src/App.cpp
@@ -1,14 +1,34 @@
 #include "App.h"
 
-App::App()
-{
-	initWindow();
-	loadData();
-}
-
 void App::run()
 {
-	NeuralNet net({ 784U, 32U, 16U, 10U }, 1.0, 32U);
+	std::cout << "Loading data...\n";
+
+	auto trainInputs = Utils::MNISTdataLoader::readMnistImages(
+		"handwritten digits data//train-images-idx3-ubyte"
+	);
+	auto trainLabels = Utils::MNISTdataLoader::readMnistLabels(
+		"handwritten digits data//train-labels-idx1-ubyte"
+	);
+
+	auto testInputs = Utils::MNISTdataLoader::readMnistImages(
+		"handwritten digits data//t10k-images-idx3-ubyte"
+	);
+	auto testLabels = Utils::MNISTdataLoader::readMnistLabels(
+		"handwritten digits data//t10k-labels-idx1-ubyte"
+	);
+
+	std::cout << "\nThis is how 2 first data points look like:\n";
+
+	Utils::MNISTdataLoader::showData(trainInputs, trainLabels, { 0U, 1U });
+
+	Utils::MNISTdataLoader::preprocess(trainInputs);
+	Utils::MNISTdataLoader::preprocess(testInputs);
+
+	auto reorganisedTrainLabels = Utils::MNISTdataLoader::reorganizeLabels(trainLabels);
+	auto reorganisedTestLabels = Utils::MNISTdataLoader::reorganizeLabels(testLabels);
+
+	NeuralNet net({ 784U, 16U, 12U, 10U }, 1.0, 32U);
 
 	unsigned epochsCount = 1U;
 	std::cout << "Learning epochs count: ";
@@ -16,93 +36,17 @@ void App::run()
 
 	for (int e = 1; e <= epochsCount; e++)
 	{
-		for (int i = 0; i < m_trainInputs.size(); i++)
+		for (int i = 0; i < trainInputs.size(); i++)
 		{
-			net.trainingStep(m_trainInputs[i], m_trainLabels[i]);
+			net.trainingStep(trainInputs[i], reorganisedTrainLabels[i]);
 		}
 
 		std::cout << "Accuracy after " << e << " epoch: " << Utils::validateClassification(
-			m_testInputs,
-			m_testLabels,
+			testInputs,
+			reorganisedTestLabels,
 			net
 		) << '\n';
 
-		Utils::randomShuffle(m_trainInputs, m_trainLabels);
+		Utils::randomShuffle(trainInputs, reorganisedTrainLabels);
 	}
-}
-
-void App::initWindow()
-{
-	std::string path = "config/graphics.ini";
-
-	std::ifstream file(path);
-
-	if (!file.is_open())
-	{
-		std::cerr << "CANNOT OPEN: " << path << '\n';
-		exit(-17);
-	}
-
-	std::string title;
-	unsigned width, height;
-	unsigned frameRateLimit;
-	bool verticalSyncEnabled;
-	sf::Vector2i position;
-
-	file >> title;
-	file >> width >> height;
-	file >> frameRateLimit;
-	file >> verticalSyncEnabled;
-	file >> position.x >> position.y;
-
-	file.close();
-
-	m_window.create(sf::VideoMode(width, height), title);
-	m_window.setFramerateLimit(frameRateLimit);
-	m_window.setVerticalSyncEnabled(verticalSyncEnabled);
-	m_window.setPosition(position);
-}
-
-void App::loadData()
-{
-	std::cout << "Loading data...\n";
-
-	m_trainInputs = Utils::MNISTdataLoader::readMnistImages(
-		"handwritten digits data//train-images-idx3-ubyte"
-	);
-	auto trainLabels = Utils::MNISTdataLoader::readMnistLabels(
-		"handwritten digits data//train-labels-idx1-ubyte"
-	);
-
-	m_testInputs = Utils::MNISTdataLoader::readMnistImages(
-		"handwritten digits data//t10k-images-idx3-ubyte"
-	);
-	auto testLabels = Utils::MNISTdataLoader::readMnistLabels(
-		"handwritten digits data//t10k-labels-idx1-ubyte"
-	);
-
-	Utils::MNISTdataLoader::preprocess(m_trainInputs);
-	Utils::MNISTdataLoader::preprocess(m_testInputs);
-
-	m_trainLabels = Utils::MNISTdataLoader::reorganizeLabels(trainLabels);
-	m_testLabels = Utils::MNISTdataLoader::reorganizeLabels(testLabels);
-
-	std::cout << "\nThis is how 2 first data points look like:\n";
-
-	Utils::MNISTdataLoader::showData(m_trainInputs, trainLabels, { 0U, 1U });
-}
-
-void App::update()
-{
-
-}
-
-void App::updateDt()
-{
-
-}
-
-void App::render()
-{
-
 }
--- Small Neural Net Visualization/src/App.h
+++ Small Neural Net Visualization/src/App.h
@@ -2,28 +2,11 @@
 
 #include "Utils.h"
 
-#include "SFML/Graphics.hpp"
-
 class App
 {
 public:
-	App();
-
 	void run();
 
 private:
-	void initWindow();
-	void loadData();
 
-	void update();
-	void updateDt();
-	void render();
-
-	sf::RenderWindow m_window;
-
-	std::vector<std::vector<Scalar>> m_trainInputs;
-	std::vector<std::vector<Scalar>> m_trainLabels;
-
-	std::vector<std::vector<Scalar>> m_testInputs;
-	std::vector<std::vector<Scalar>> m_testLabels;
 };
