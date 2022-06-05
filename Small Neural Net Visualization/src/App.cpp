8e4d98c85d8c996ebdd8cdb6e3b14ffe858bad50
MateuszKozlowski1124 <70546149+MateuszKozlowski1124@users.noreply.github.com>
2022-06-05 19:33:24 +0200

    Move loading data to App::loadData and add 3 empty functions

--- Small Neural Net Visualization/src/App.cpp
+++ Small Neural Net Visualization/src/App.cpp
@@ -9,32 +9,6 @@ App::App()
 
 void App::run()
 {
-	std::cout << "Loading data...\n";
-
-	auto trainInputs = Utils::MNISTdataLoader::readMnistImages(
-		"handwritten digits data//train-images-idx3-ubyte"
-	);
-	auto trainLabels = Utils::MNISTdataLoader::readMnistLabels(
-		"handwritten digits data//train-labels-idx1-ubyte"
-	);
-
-	auto testInputs = Utils::MNISTdataLoader::readMnistImages(
-		"handwritten digits data//t10k-images-idx3-ubyte"
-	);
-	auto testLabels = Utils::MNISTdataLoader::readMnistLabels(
-		"handwritten digits data//t10k-labels-idx1-ubyte"
-	);
-
-	std::cout << "\nThis is how 2 first data points look like:\n";
-
-	Utils::MNISTdataLoader::showData(trainInputs, trainLabels, { 0U, 1U });
-
-	Utils::MNISTdataLoader::preprocess(trainInputs);
-	Utils::MNISTdataLoader::preprocess(testInputs);
-
-	auto reorganisedTrainLabels = Utils::MNISTdataLoader::reorganizeLabels(trainLabels);
-	auto reorganisedTestLabels = Utils::MNISTdataLoader::reorganizeLabels(testLabels);
-
 	NeuralNet net({ 784U, 16U, 12U, 10U }, 1.0, 32U);
 
 	unsigned epochsCount = 1U;
@@ -45,18 +19,18 @@ void App::run()
 
 	for (int e = 1; e <= epochsCount; e++)
 	{
-		for (int i = 0; i < trainInputs.size(); i++)
+		for (int i = 0; i < m_trainInputs.size(); i++)
 		{
-			net.trainingStep(trainInputs[i], reorganisedTrainLabels[i]);
+			net.trainingStep(m_trainInputs[i], m_trainLabels[i]);
 		}
 
 		std::cout << "Accuracy after " << e << " epoch: " << Utils::validateClassification(
-			testInputs,
-			reorganisedTestLabels,
+			m_testInputs,
+			m_testLabels,
 			net
 		) << '\n';
 
-		Utils::randomShuffle(trainInputs, reorganisedTrainLabels);
+		Utils::randomShuffle(m_trainInputs, m_trainLabels);
 	}
 }
 
@@ -67,10 +41,49 @@ void App::initWindow()
 
 void App::loadData()
 {
+	std::cout << "Loading data...\n";
 
+	m_trainInputs = Utils::MNISTdataLoader::readMnistImages(
+		"handwritten digits data//train-images-idx3-ubyte"
+	);
+	auto trainLabels = Utils::MNISTdataLoader::readMnistLabels(
+		"handwritten digits data//train-labels-idx1-ubyte"
+	);
+
+	m_testInputs = Utils::MNISTdataLoader::readMnistImages(
+		"handwritten digits data//t10k-images-idx3-ubyte"
+	);
+	auto testLabels = Utils::MNISTdataLoader::readMnistLabels(
+		"handwritten digits data//t10k-labels-idx1-ubyte"
+	);
+
+	std::cout << "\nThis is how 2 first data points look like:\n";
+
+	Utils::MNISTdataLoader::showData(m_trainInputs, trainLabels, { 0U, 1U });
+
+	Utils::MNISTdataLoader::preprocess(m_trainInputs);
+	Utils::MNISTdataLoader::preprocess(m_testInputs);
+
+	m_trainLabels = Utils::MNISTdataLoader::reorganizeLabels(trainLabels);
+	m_testLabels = Utils::MNISTdataLoader::reorganizeLabels(testLabels);
 }
 
 void App::initNet()
+{
+
+}
+
+void App::update()
+{
+
+}
+
+void App::updateDt()
+{
+
+}
+
+void App::render()
 {
 
 }
--- Small Neural Net Visualization/src/App.h
+++ Small Neural Net Visualization/src/App.h
@@ -13,4 +13,14 @@ private:
 	void initWindow();
 	void loadData();
 	void initNet();
+
+	void update();
+	void updateDt();
+	void render();
+
+	std::vector<std::vector<Scalar>> m_trainInputs;
+	std::vector<std::vector<Scalar>> m_trainLabels;
+
+	std::vector<std::vector<Scalar>> m_testInputs;
+	std::vector<std::vector<Scalar>> m_testLabels;
 };
